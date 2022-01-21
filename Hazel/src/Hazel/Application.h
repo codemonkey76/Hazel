27edfbf376ec9fe05f56e65b3ddb8829c0c3d1c9
Shane Poppleton <shane@bjja.com.au>
2022-01-22 07:35:51 +1000

    RendererAPI Abstraction

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -6,9 +6,11 @@
 #include "Hazel/LayerStack.h"
 #include "Hazel/Events/Event.h"
 #include "Hazel/Events/ApplicationEvent.h"
-#include "Hazel/Renderer/Shader.h"
 
 #include "Hazel/ImGui/ImGuiLayer.h"
+
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/Buffer.h"
 
 namespace Hazel {
 
@@ -36,9 +38,11 @@ namespace Hazel {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
