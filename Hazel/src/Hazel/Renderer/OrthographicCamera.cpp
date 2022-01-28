6d20108f0f52b2ae9da25739ca1d24bce2745633
Shane Poppleton <shane@bjja.com.au>
2022-01-28 11:53:57 +1000

    Camera Controllers

--- Hazel/src/Hazel/Renderer/OrthographicCamera.cpp
+++ Hazel/src/Hazel/Renderer/OrthographicCamera.cpp
@@ -4,11 +4,18 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 namespace Hazel {
+	
 	OrthographicCamera::OrthographicCamera(float left, float right, float bottom, float top)
 		: m_ProjectionMatrix(glm::ortho(left, right, bottom, top, -1.0f, 1.0f)), m_ViewMatrix(1.0f)
 	{
 		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
 	}
+
+    void OrthographicCamera::SetProjection(float left, float right, float bottom, float top)
+    {
+		m_ProjectionMatrix = glm::ortho(left, right, bottom, top, -1.0f, 1.0f);
+		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
+    }
 
 	void OrthographicCamera::RecalculateViewMatrix()
 	{
