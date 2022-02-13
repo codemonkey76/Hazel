5049e2caa8821e4af61c8e53a2aa185942c161f5
Shane Poppleton <shane@bjja.com.au>
2022-02-14 08:32:34 +1000

    Add dockspace

--- Hazel/src/Hazel/Renderer/Texture.h
+++ Hazel/src/Hazel/Renderer/Texture.h
@@ -13,6 +13,7 @@ namespace Hazel {
 
 			virtual uint32_t GetWidth() const = 0;
 			virtual uint32_t GetHeight() const = 0;
+			virtual uint32_t GetRendererID() const = 0;
 			
 			virtual bool operator==(const Texture& other) const = 0;
 
