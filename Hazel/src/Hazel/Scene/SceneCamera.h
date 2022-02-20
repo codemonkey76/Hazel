ca9b305277a08ff69bbe90d0245a726eb70fcf33
Shane Poppleton <shane@bjja.com.au>
2022-02-20 10:58:51 +1000

    Camera Component UI

--- Hazel/src/Hazel/Scene/SceneCamera.h
+++ Hazel/src/Hazel/Scene/SceneCamera.h
@@ -6,23 +6,49 @@ namespace Hazel {
 
 	class SceneCamera : public Camera
 	{
-	public:
-		SceneCamera();
-		virtual ~SceneCamera() = default;
+		public:
+			enum class ProjectionType { Perspective = 0, Orthographic = 1 };
 
-		void SetOrthographic(float size, float nearClip, float farClip);
+		public:
+			SceneCamera();
+			virtual ~SceneCamera() = default;
 
-		void SetViewportSize(uint32_t width, uint32_t height);
+			void SetOrthographic(float size, float nearClip, float farClip);
+			void SetPerspective(float verticalFov, float nearClip, float farClip);
 
-		float GetOrthographicSize() const { return m_OrthographicSize; }
-		void SetOrthographicSize(float size) { m_OrthographicSize = size; RecalculateProjection(); }
-	private:
-		void RecalculateProjection();
-	private:
-		float m_OrthographicSize = 10.0f;
-		float m_OrthographicNear = -1.0f, m_OrthographicFar = 1.0f;
+			void SetViewportSize(uint32_t width, uint32_t height);
 
-		float m_AspectRatio = 0.0f;
+			float GetPerspectiveVerticalFOV() const { return m_PerspectiveFOV; }
+			void SetPerspectiveVerticalFOV(float verticalFov) { m_PerspectiveFOV = verticalFov; RecalculateProjection(); }
+			float GetPerspectiveNearClip() const { return m_PerspectiveNear; }
+			void SetPerspectiveNearClip(float nearClip) { m_PerspectiveNear = nearClip; RecalculateProjection(); }
+			float GetPerspectiveFarClip() const { return m_PerspectiveFar; }
+			void SetPerspectiveFarClip(float farClip) { m_PerspectiveFar = farClip;  RecalculateProjection(); }
+
+			float GetOrthographicSize() const { return m_OrthographicSize; }
+			void SetOrthographicSize(float size) { m_OrthographicSize = size; RecalculateProjection(); }
+			float GetOrthographicNearClip() const { return m_OrthographicNear; }
+			void SetOrthographicNearClip(float nearClip) { m_OrthographicNear = nearClip; RecalculateProjection(); }		
+			float GetOrthographicFarClip() const { return m_OrthographicFar; }
+			void SetOrthographicFarClip(float farClip) { m_OrthographicFar = farClip;  RecalculateProjection(); }
+			
+			ProjectionType GetProjectionType() { return m_ProjectionType; }
+			void SetProjectionType(ProjectionType type) { m_ProjectionType = type; RecalculateProjection(); }
+
+		private:
+			void RecalculateProjection();
+
+		private:
+			ProjectionType m_ProjectionType = ProjectionType::Orthographic;
+
+			float m_OrthographicSize = 10.0f;
+			float m_OrthographicNear = -1.0f, m_OrthographicFar = 1.0f;
+
+			float m_PerspectiveFOV = glm::radians(45.0f);
+			float m_PerspectiveNear = 0.01f, m_PerspectiveFar = 1000.0f;
+			
+			float m_AspectRatio = 0.0f;
+
 	};
 
 }
