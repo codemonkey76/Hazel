6ce14dc8c95baaf279dfa9f8f79ba96fc4a8def0
Shane Poppleton <shane@bjja.com.au>
2022-02-24 06:58:31 +1000

    Editor Camera

--- Hazel/src/Hazel/Scene/Scene.h
+++ Hazel/src/Hazel/Scene/Scene.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "entt.hpp"
-
-
 #include "Hazel/Core/Timestep.h"
+#include "Hazel/Renderer/EditorCamera.h"
+
+#include "entt.hpp"
 
 namespace Hazel {
 
@@ -11,27 +11,29 @@ namespace Hazel {
 
 	class Scene
 	{
-	public:
-		Scene();
-		~Scene();
+		public:
+			Scene();
+			~Scene();
 
-		Entity CreateEntity(const std::string& name = std::string());
-		void DestroyEntity(Entity entity);
+			Entity CreateEntity(const std::string& name = std::string());
+			void DestroyEntity(Entity entity);
 
-		void OnUpdate(Timestep ts);
-		void OnViewportResize(uint32_t width, uint32_t height);
+			void OnUpdateRuntime(Timestep ts);
+			void OnUpdateEditor(Timestep ts, EditorCamera& camera);
 
-		Entity GetPrimaryCameraEntity();
-	private:
-		template<typename T>
-		void OnComponentAdded(Entity entity, T& component);
-	private:
-		entt::registry m_Registry;
-		uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+			void OnViewportResize(uint32_t width, uint32_t height);
 
-		friend class Entity;
-		friend class SceneSerializer;
-		friend class SceneHierarchyPanel;
+			Entity GetPrimaryCameraEntity();
+		private:
+			template<typename T>
+			void OnComponentAdded(Entity entity, T& component);
+		private:
+			entt::registry m_Registry;
+			uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
+
+			friend class Entity;
+			friend class SceneSerializer;
+			friend class SceneHierarchyPanel;
 	};
 
 }
--- Hazelnut/src/EditorLayer.h
+++ Hazelnut/src/EditorLayer.h
@@ -2,6 +2,7 @@
 
 #include "Hazel.h"
 #include "Panels/SceneHierarchyPanel.h"
+#include "Hazel/Renderer/EditorCamera.h"
 
 namespace Hazel {
 
@@ -40,6 +41,7 @@ namespace Hazel {
 			Entity m_SecondCamera;
 
 			bool m_PrimaryCamera = true;
+			EditorCamera m_EditorCamera;
 
 			Ref<Texture2D> m_CheckerboardTexture;
 
