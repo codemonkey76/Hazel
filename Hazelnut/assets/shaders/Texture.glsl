80840315edcdfadbad1aacda5661db5ce9764190
Shane Poppleton <shane@bjja.com.au>
2022-02-25 09:21:15 +1000

    Preparing framebuffers for mouse picking

--- Hazelnut/assets/shaders/Texture.glsl
+++ Hazelnut/assets/shaders/Texture.glsl
@@ -29,7 +29,7 @@ void main()
 #version 330 core
 
 layout(location = 0) out vec4 color;
-layout(location = 1) out vec4 color2;
+layout(location = 1) out int color2;
 
 in vec4 v_Color;
 in vec2 v_TexCoord;
@@ -78,5 +78,5 @@ void main()
 	}
 	color = texColor;
 
-	color2 = vec4(0.9, 0.2, 0.3, 1.0);
+	color2 = 50; // placeholder for our entity ID
 }
