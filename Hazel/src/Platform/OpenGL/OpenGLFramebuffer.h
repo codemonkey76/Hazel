4391098a29807f1d0d55d9121cb5cd180a49f75d
Shane Poppleton <shane@bjja.com.au>
2022-02-25 08:44:47 +1000

    Multiple Render Targets

--- Hazel/src/Platform/OpenGL/OpenGLFramebuffer.h
+++ Hazel/src/Platform/OpenGL/OpenGLFramebuffer.h
@@ -6,24 +6,31 @@ namespace Hazel {
 
 	class OpenGLFramebuffer : public Framebuffer
 	{
-	public:
-		OpenGLFramebuffer(const FramebufferSpecification& spec);
-		virtual ~OpenGLFramebuffer();
 
-		void Invalidate();
+		public:
+			OpenGLFramebuffer(const FramebufferSpecification& spec);
+			virtual ~OpenGLFramebuffer();
 
-		virtual void Bind() override;
-		virtual void Unbind() override;
+			void Invalidate();
 
-		virtual void Resize(uint32_t width, uint32_t height) override;
+			virtual void Bind() override;
+			virtual void Unbind() override;
 
-		virtual uint32_t GetColorAttachmentRendererID() const override { return m_ColorAttachment; }
+			virtual void Resize(uint32_t width, uint32_t height) override;
 
-		virtual const FramebufferSpecification& GetSpecification() const override { return m_Specification; }
-	private:
-		uint32_t m_RendererID = 0;
-		uint32_t m_ColorAttachment = 0, m_DepthAttachment = 0;
-		FramebufferSpecification m_Specification;
+			virtual uint32_t GetColorAttachmentRendererID(uint32_t index = 0) const override { HZ_CORE_ASSERT(index < m_ColorAttachments.size());  return m_ColorAttachments[index]; }
+
+			virtual const FramebufferSpecification& GetSpecification() const override { return m_Specification; }
+
+		private:
+			uint32_t m_RendererID = 0;
+			FramebufferSpecification m_Specification;
+
+			std::vector<FramebufferTextureSpecification> m_ColorAttachmentSpecifications;
+			FramebufferTextureSpecification m_DepthAttachmentSpecification = FramebufferTextureFormat::None;
+
+			std::vector<uint32_t> m_ColorAttachments;
+			uint32_t m_DepthAttachment = 0;
 	};
 
 }
